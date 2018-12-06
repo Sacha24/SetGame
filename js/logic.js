5b0c5f6165b7bd94f921c41fa212f4606eb0f586
Solal Cohen <solalcohen@MacBook-Pro-de-Solal.local>
2018-12-06 12:07:13 +0200

    random function

--- js/logic.js
+++ js/logic.js
@@ -1,22 +1,24 @@
 var setGame = {};
-var shapeNumber = [1,2,3];
-var symbol=['menorah','snowman','deer'];
-var color=['red','green','purple'];
+var shapeNumber = [1,2,3,1,2,3,1,2,3,1,2,3];
+var symbol=['menorah','snowman','deer','menorah','snowman','deer','menorah','snowman','deer','menorah','snowman','deer'];
+var color=['red','green','purple','red','green','purple','red','green','purple','red','green','purple'];
 
 var Cards= dispatch()
 
 function dispatch () {
-    var cardN=[];
-    for(var i=0;i<shapeNumber.length;i++){
-        cardN[i].shape = shapeNumber[Math.floor(Math.random() * shapeNumber.length)];
+    var cardRet=[];
+    for(var i=12;i>0;i--){
+        var cardN = {};
+        cardN.shape = shapeNumber[Math.floor(Math.random() * shapeNumber.length)];
         var indexShape=shapeNumber.indexOf(cardN.shape);
         shapeNumber.splice(indexShape, 1);
-        cardN[i].symbol= symbol[Math.floor(Math.random()*symbol.length)];
+        cardN.symbol= symbol[Math.floor(Math.random()*symbol.length)];
         var indexSymbol=symbol.indexOf(cardN.symbol);
         symbol.splice(indexSymbol, 1);
-        cardN[i].color= color[Math.floor(Math.random()*color.length)];
+        cardN.color= color[Math.floor(Math.random()*color.length)];
         var indexColor=color.indexOf(cardN.color);
         color.splice(indexColor, 1);
-    }return cardN;
+        cardRet.push(cardN);
+    }return cardRet;
 
 }
