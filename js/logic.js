686d9047e2d72e02394c664c2e8510d0030f3c45
Axel <axelpriven@gmail.com>
2018-12-06 13:03:29 +0200

    egg shape

--- js/logic.js
+++ js/logic.js
@@ -46,13 +46,6 @@ function isMatch(card1, card2, card3) {
     card2.color != card3.color ? null : diffColor++;
     card1.color != card3.color ? null : diffColor++;
 
-<<<<<<< HEAD
-    card1.diffborder != card2.diffborder ? null : diffborder++;
-    card2.diffborder != card3.diffborder ? null : diffborder++;
-    card1.diffborder != card3.diffborder ? null : diffborder++;
-
-    return diffColor + diffSymbol + diffShape +diffborder == 0 ? true : false;
-=======
     card1.shading != card2.shading ? null : diffShading++;
     card2.shading != card3.shading ? null : diffShading++;
     card1.shading != card3.shading ? null : diffShading++;
@@ -62,5 +55,4 @@ function isMatch(card1, card2, card3) {
     } else {
         return false;
     }
->>>>>>> 806e7e07c0d27f1de0818310a8ca08e28357dd1c
 }
